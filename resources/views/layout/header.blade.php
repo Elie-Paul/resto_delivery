2b59b1525e60e519aa08b5d41208d6a265ad77ed
Elie-Paul <eliepaulmoubotouto@gmail.com>
2019-09-18 14:41:18 +0000

    first commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Aahar || Food Delivery Html5 Template</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+	<link rel="apple-touch-icon" href="{{ asset('images/icon.png') }}">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/plugins.css') }}">
+	<link rel="stylesheet" href="{{ asset('style.css') }}">
+
+	<!-- Cusom css -->
+ <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+ <!-- Modernizer js -->
+ <script src="{{ asset('js/vendor/modernizr-3.5.0.min.js') }}"></script>
+</head>
+<body>
+
+    @include('layout.nav')
